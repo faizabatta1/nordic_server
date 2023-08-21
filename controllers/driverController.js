dd650a9c03b8ebf7f2d2705ecb695f52989fb2cc
ali99944 <ali99944@github.com>
2023-08-21 03:53:36 +0300

    update

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -115,10 +115,10 @@ const createNewDriver = async (req,res) =>{
 
 
         await browser.close();
-        res.status(200).json({ message: 'PDF generated and saved successfully' });
+        return res.status(200).json({ message: 'PDF generated and saved successfully' });
     }catch (error){
         console.log(error.message)
-        res.status(500).send(error.message)
+        return res.status(500).send(error.message)
     }
 }
 
