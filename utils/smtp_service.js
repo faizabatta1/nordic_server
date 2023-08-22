b3075545a3fb8153d9781fc512977aba26eba40f
ali99944 <ali99944@github.com>
2023-08-22 08:43:03 +0300

    upload

---
+++ utils/smtp_service.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST, // Hostinger SMTP server
+  port: +process.env.SMTP_PORT, // Port for sending mail
+  secure: true, // Use SSL (false for TLS)
+  auth: {
+    user: process.env.SMTP_USER, // Your email address
+    pass: process.env.SMTP_PASS, 
+  },
+});
+
+
+function sendAlertMail({ subject, text, to }){
+  const mailOptions = {
+    from: 'wogood@bilsjekk.in', // Sender address
+    to: to, // Recipient address
+    subject: subject,
+    text: text,
+  };
+  
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+}
+
+module.exports = {
+  sendAlertEmail
+}
