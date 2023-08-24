2b5fc756de600ce31683bb12be6cbc4350710a3e
ali99944 <ali99944@github.com>
2023-08-24 19:05:45 +0300

    upload

--- routes/settingsFront.js
+++ routes/settingsFront.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/settings',(req,res) =>{
-  return res.status(200).render('/settings/index')
+  return res.status(200).render('settings/index')
 })
 
 
