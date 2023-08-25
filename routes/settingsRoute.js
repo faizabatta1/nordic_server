6c52adeceed3890354bda2d2478da6035b40d1cf
ali99944 <ali99944@github.com>
2023-08-25 08:00:06 +0300

    upload

--- routes/settingsRoute.js
+++ routes/settingsRoute.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
+const fs = require('fs')
 
 router.put('/settings/credentials', async (req,res) =>{
   const { username,password } = req.body
 
-  console.log(username)
-  console.log(password)
+  const data = JSON.stringify({
+    username: username,
+    password: password
+  })
+  fs.writeFileSync(path.join(__dirname,'../data/credentials.json'), data,{
+    encoding:'utf-8'
+  });
 
   return res.sendStatus(200)
 })
