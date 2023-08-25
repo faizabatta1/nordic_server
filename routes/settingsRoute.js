8aeb9dd8b750d5f484ca489d297b5971f13e99e5
ali99944 <ali99944@github.com>
2023-08-25 09:03:45 +0300

    upload

--- routes/settingsRoute.js
+++ routes/settingsRoute.js
@@ -4,25 +4,56 @@ const path = require('path')
 const fs = require('fs')
 
 router.put('/settings/credentials', async (req,res) =>{
-  const { username,password } = req.body
+  try{
+    const { username,password } = req.body
 
-  const data = JSON.stringify({
-    username: username,
-    password: password
-  })
-  fs.writeFileSync(path.join(__dirname,'../data/credentials.json'), data,{
-    encoding:'utf-8'
-  });
-
-  return res.sendStatus(200)
+    const data = JSON.stringify({
+      username: username,
+      password: password
+    })
+    fs.writeFileSync(path.join(__dirname,'../data/credentials.json'), data,{
+      encoding:'utf-8'
+    });
+  
+    return res.sendStatus(200)
+  }catch(error){
+    return res.send(500).send(error.message)
+  }
 })
 
 router.put('/settings/emailTemplate', async (req,res) =>{
+  try{
+    const { subject,text } = req.body
+
+    const data = JSON.stringify({
+      subject: subject,
+      text: text
+    })
+    fs.writeFileSync(path.join(__dirname,'../data/email.json'), data,{
+      encoding:'utf-8'
+    });
   
+    return res.sendStatus(200)
+  }catch(error){
+    return res.send(500).send(error.message)
+  }
 })
 
 router.put('/settings/smsTable', async (req,res) =>{
+  try{
+    const { text } = req.body
+
+    const data = JSON.stringify({
+      text: text,
+    })
+    fs.writeFileSync(path.join(__dirname,'../data/sms.json'), data,{
+      encoding:'utf-8'
+    });
   
+    return res.sendStatus(200)
+  }catch(error){
+    return res.send(500).send(error.message)
+  }
 })
 
 module.exports = router
