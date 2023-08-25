b443f7e443ebc7e329ba01ff11b7fd0531bc851e
ali99944 <ali99944@github.com>
2023-08-25 07:48:51 +0300

    upload

--- routes/settingsRoute.js
+++ routes/settingsRoute.js
@@ -3,7 +3,11 @@ const router = express.Router()
 
 router.put('/settings/credentials', async (req,res) =>{
   const { username,password } = req.body
-  
+
+  console.log(username)
+  console.log(password)
+
+  return res.sendStatus(200)
 })
 
 router.put('/settings/emailTemplate', async (req,res) =>{
