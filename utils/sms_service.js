9f3740d91ebc472f30171a092a360b1b9446b3bd
ali99944 <ali99944@github.com>
2023-08-26 10:29:12 +0300

    upload

--- utils/sms_service.js
+++ utils/sms_service.js
@@ -1,30 +1,38 @@
 const { Vonage } = require('@vonage/server-sdk');
 const fs = require('fs')
 const path = require('path')
+const axios = require('axios');
+
 
 // const vonage = new Vonage({
+//   apiKey: "d1840a73",
+//   apiSecret: "c2eOlaZYWqqO6DyT",
 //   applicationId: "b3cc5981-a376-4298-8921-03cb678a6fcf",
-//   // privateKey: fs.readFileSync(path.join(__dirname,'private.key'),{
-//   //   encoding:'utf-8'
-//   // })
-
-//   privateKey: './private.key'
+//   // privateKey:"./private.key",
+//   privateKey: Buffer.from(process.env.VONAGE_APPLICATION_PRIVATE_KEY64, 'base64').toString(),
 // });
 
-const vonage = new Vonage({
-  apiKey: "d1840a73",
-  apiSecret: "c2eOlaZYWqqO6DyT",
-  applicationId: "b3cc5981-a376-4298-8921-03cb678a6fcf",
-  // privateKey:"./private.key",
-  privateKey: Buffer.from(process.env.VONAGE_APPLICATION_PRIVATE_KEY64, 'base64').toString(),
-});
+async function sendAlertSMS(text,to){   
 
-async function sendAlertSMS(text,to){ 
+const apiUrl = 'https://rest.nexmo.com/sms/json';
+const apiKey = 'd1840a73';
+const apiSecret = 'c2eOlaZYWqqO6DyT';
 
-  const resp = await vonage.sms.send({
-    to: '201150421159',
-    from: '4740088605',
-    text: 'pn167 har oppdaterer kjøretøyets EC876765, tjenestenr er: EL 03  kjørelengde 2000 og dette er den planlagte servicedatoen.',
+const postData = {
+  api_key: apiKey,
+  api_secret: apiSecret,
+  type: 'text',
+  from: 'Nordic',
+  to: '4746231130',
+  text: text
+};
+
+axios.post(apiUrl, postData)
+  .then(response => {
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
   });
 }
 
