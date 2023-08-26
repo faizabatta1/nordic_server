5b52d494043ceb59f00e543faf8a799ff7288429
ali99944 <ali99944@github.com>
2023-08-26 18:31:21 +0300

    upload

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -45,7 +45,7 @@ const createNewDriver = async (req,res) =>{
 
 
             sendAlertMail({
-                to:'vaktleder@parknordic.no',
+                to:'me@mutaz.no',
                 subject: emailSubject,
                 text: emailText,
                 html: `<h2>${emailText}</h2>`                    
@@ -67,7 +67,7 @@ const createNewDriver = async (req,res) =>{
 
             await sendAlertSMS({
                 text: smsText,
-                to:'4747931499'
+                to:'4740088605'
             });
 
             await Car.findOneAndUpdate({ _id: information.carId },{
