1fdbdea17f3f06b1ecc994adb870cee9e7e52504
ali99944 <ali99944@github.com>
2023-08-27 16:58:20 +0300

    upload

---
+++ routes/information.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/info/car',async (req,res) =>{
+  try{
+    let applicationData = fs.readFileSync(path.join(__dirname,'../data/application.json'),{ 
+      encoding: 'utf8',
+      flag: 'r'
+     })
+     let applicationJson = JSON.parse(applicationData)
+
+     return res.status(200).send(applicationJson.car)
+  }catch(error){
+    return res.status(500).send(error.message)
+  }
+})
+
+router.get('/info/shift',async (req,res) =>{
+  try{
+    let applicationData = fs.readFileSync(path.join(__dirname,'../data/application.json'),{ 
+      encoding: 'utf8',
+      flag: 'r'
+     })
+     let applicationJson = JSON.parse(applicationData)
+
+     return res.status(200).send(applicationJson.shift)
+  }catch(error){
+    return res.status(500).send(error.message)
+  }
+})
+
+router.get('/info/kilometer',async (req,res) =>{
+  try{
+    let applicationData = fs.readFileSync(path.join(__dirname,'../data/application.json'),{ 
+      encoding: 'utf8',
+      flag: 'r'
+     })
+     let applicationJson = JSON.parse(applicationData)
+
+     return res.status(200).send(applicationJson.kilometer)
+  }catch(error){
+    return res.status(500).send(error.message)
+  }
+})
+
+router.get('/info/violation',async (req,res) =>{
+  try{
+    let applicationData = fs.readFileSync(path.join(__dirname,'../data/application.json'),{ 
+      encoding: 'utf8',
+      flag: 'r'
+     })
+     let applicationJson = JSON.parse(applicationData)
+
+     return res.status(200).send(applicationJson.violation)
+  }catch(error){
+    return res.status(500).send(error.message)
+  }
+})
+
+module.exports = router
