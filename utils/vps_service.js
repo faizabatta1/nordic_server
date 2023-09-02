ab58e1f6513c1d1f8041a48b0cfc3dfb9d1a1bb3
ali99944 <ali99944@github.com>
2023-09-02 04:16:17 +0300

    upload

--- utils/vps_service.js
+++ utils/vps_service.js
@@ -1,41 +1,35 @@
 const ssh2 = require('ssh2');
 
-let pm2Path = '/root/.nvm/versions/node/v18.0.0/bin/pm2';
+const conn = new ssh2.Client();
+let pm2Path = '/root/.nvm/versions/node/v18.0.0/bin/pm2'
 
-function createSSHConnection() {
-  const conn = new ssh2.Client();
 
-  conn.on('ready', () => {
-    console.log('Connected to VPS');
-  });
+conn.on('ready', () => {
+  console.log('Connected to VPS');
+});
 
-  conn.on('error', (err) => {
-    console.error(`Error connecting to VPS: ${err.message}`);
-  });
+conn.on('error', (err) => {
+  console.error(`Error connecting to VPS: ${err.message}`);
+});
 
-  return conn;
-}
+// Connect to your VPS using SSH
+conn.connect({
+  host: '62.72.18.83',
+  port: 22,
+  username: 'root',
+  password: 'ray01ocruwRiP=LgEvoQ',
+});
 
-function restartVPS() {
-  const conn = createSSHConnection();
-
-  conn.connect({
-    host: '62.72.18.83',
-    port: 22,
-    username: 'root',
-    password: 'ray01ocruwRiP=LgEvoQ',
-  });
-
-  // Restart your VPS by executing a command
-  conn.exec(`export PATH=$PATH:/root/.nvm/versions/node/v18.0.0/bin/ && ${pm2Path} list`, (err, stream) => {
+function restartVPS(){
+   // Restart your VPS by executing a command
+  conn.exec(`export PATH=$PATH:/root/.nvm/versions/node/v18.0.0/bin/ && ${pm2Path} start nordic`, (err, stream) => {
     if (err) {
-      console.log(err);
-      throw err;
+        console.log(err);
+        throw err
     };
-
+    
     stream.on('close', (code, signal) => {
       console.log(`Command execution completed with code ${code}`);
-      conn.end(); // Close the SSH connection
     }).on('data', (data) => {
       console.log(`Command output: ${data}`);
       return data
@@ -43,26 +37,18 @@ function restartVPS() {
   });
 }
 
-function updateNordic() {
-  const conn = createSSHConnection();
 
-  conn.connect({
-    host: '62.72.18.83',
-    port: 22,
-    username: 'root',
-    password: 'ray01ocruwRiP=LgEvoQ',
-  });
-
-  // Restart your VPS by executing a command
-  conn.exec('cd ~/workspace/Bilsjekkserverp && git pull', (err, stream) => {
+function updateNordic(){
+   // Restart your VPS by executing a command
+   conn.exec('cd ~/workspace/Bilsjekkserverp && git pull', (err, stream) => {
     if (err) {
-      console.log(err);
-      throw err;
+        console.log(err);
+        throw err
     };
-
+    
     stream.on('close', (code, signal) => {
+        console.log(signal);
       console.log(`Command execution completed with code ${code}`);
-      conn.end(); // Close the SSH connection
     }).on('data', (data) => {
       console.log(`Command output: ${data}`);
       restartVPS()
@@ -70,26 +56,17 @@ function updateNordic() {
   });
 }
 
-function stopNordic() {
-  const conn = createSSHConnection();
-
-  conn.connect({
-    host: '62.72.18.83',
-    port: 22,
-    username: 'root',
-    password: 'ray01ocruwRiP=LgEvoQ',
-  });
-
-  // Restart your VPS by executing a command
-  conn.exec('pm2 stop techs', (err, stream) => {
+function stopNordic(){
+   // Restart your VPS by executing a command
+   conn.exec('pm2 stop techs', (err, stream) => {
     if (err) {
-      console.log(err);
-      throw err;
+        console.log(err);
+        throw err
     };
-
+    
     stream.on('close', (code, signal) => {
+        console.log(signal);
       console.log(`Command execution completed with code ${code}`);
-      conn.end(); // Close the SSH connection
     }).on('data', (data) => {
       console.log(`Command output: ${data}`);
     });
@@ -99,5 +76,5 @@ function stopNordic() {
 module.exports = {
   stopNordic,
   updateNordic,
-  restartVPS
+  restartVPS 
 }
