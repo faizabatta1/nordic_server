39b46a76c6efb29cf30579cef1fa15888f60935a
ali99944 <ali99944@github.com>
2023-09-02 21:44:28 +0300

    upload

--- routes/vpsRouter.js
+++ routes/vpsRouter.js
@@ -7,7 +7,6 @@ const {
   prepareBackup
 } = require('../utils/vps_service')
 
-const { downloadBackup } = require('../utils/scp_backup')
 
 router.get('/vps/restart',(req,res) =>{
   try{
@@ -30,15 +29,6 @@ router.get('/vps/update',(req,res) =>{
 router.get('/vps/backup/prepare',async (req,res) =>{
   try{
     prepareBackup()
-    return res.sendStatus(200)
-  }catch(error){
-    return res.status(500).send(error.message)
-  }
-})
-
-router.get('/vps/backup/download',async (req,res) =>{
-  try{
-    await downloadBackup();
     return res.sendStatus(200)
   }catch(error){
     return res.status(500).send(error.message)
