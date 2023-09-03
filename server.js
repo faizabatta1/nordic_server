a6bd99f9a2230d0363c5301b2c5b655ed90bd038
ali99944 <ali99944@github.com>
2023-09-04 01:04:06 +0300

    upload

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ require('./utils/mongodbConnection')
 const qr = require('qr-image');
 const fs = require('fs')
 
-const http = require('http');
+const https = require('https');
 const socketIo = require('socket.io');
 
 const express = require('express')
 const app = express()
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = socketIo(server);
 
 // WebSocket connection handling
