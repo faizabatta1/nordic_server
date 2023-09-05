ef57eb7c329d389846be49fab475768e144b0f10
ali99944 <ali99944@github.com>
2023-09-06 01:47:01 +0300

    upload

--- models/PostalViolation.js
+++ models/PostalViolation.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const postalViolationSchema = new mongoose.Schema({
   violationNumber:{
-    type: Number,
+    type: String,
     required: true
   },
   pnid:{
