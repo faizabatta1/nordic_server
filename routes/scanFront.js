b1d522c145dd0f9137da2d471780f489290476be
ali99944 <ali99944@github.com>
2023-09-09 02:44:51 +0300

    upload

--- routes/scanFront.js
+++ routes/scanFront.js
@@ -19,7 +19,7 @@ router.get('/scans/:id', async (req, res) => {
       if (!scan) {
           return res.status(404).json({ error: 'PDF not found' });
       }
-      return res.status(200).render('scans/postal_show', { postal });
+      return res.status(200).render('scans/postal_show', { scan });
   } catch (error) {
     console.log(error.message)
       return res.status(500).json({ error: 'Internal Server Error' });
